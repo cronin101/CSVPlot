b63916c9b1086a2596a8959fcc4aaca56d74601a
Aaron Cronin <cronin@include.cat>
2014-06-05 23:33:11 +0100

    Axes labled

--- plot_csv.py
+++ plot_csv.py
@@ -12,8 +12,12 @@ cmap = mpl.colors.ListedColormap(['r', '1', '0.5', '0'])
 
 # Draw matrix as 'pcolot' plot
 grid = pp.pcolor(matrix, cmap=cmap)
+
 pp.xlim(xmax=matrix.shape[1] - 1)
+pp.xlabel('Time')
+
 pp.ylim(ymin=1, ymax=matrix.shape[0])
+pp.ylabel('State')
 
 # Add key
 pp.colorbar(boundaries=[x + 0.5 for x in range(4)], ticks=range(4))
